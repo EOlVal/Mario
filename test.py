22444185497a2636bae3f1a4e6b208d6b98ef1b3
EOlVal <128788004+EOlVal@users.noreply.github.com>
2024-02-04 00:46:19 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-spisok = []
-result = 0
-for x in range(1, 1000):
-    if len(str(x)) == 3 and x % 2 == 0 and str(x)[-1] != '8' and x % 3 != 0:
-        spisok.append(x)
-result += (spisok[0] + spisok[-1])
-print(result)
